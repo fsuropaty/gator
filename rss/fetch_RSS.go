5eecef9b15a7a483959d0b08ede1129225a3258f
fsuropaty <fauzy.ramadhany@gmail.com>
2025-01-11 16:59:45 +0900

    feat : fetch RSS feed

---
+++ rss/fetch_RSS.go
@@ -0,0 +1,50 @@
+package rss
+
+import (
+	"context"
+	"encoding/xml"
+	"fmt"
+	"html"
+	"io"
+	"net/http"
+)
+
+func FetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't make a new request: %w", err)
+	}
+	req.Header.Set("User-Agent", "gator")
+
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't complete the HTTP request: %w", err)
+	}
+
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		return nil, fmt.Errorf("Received non-OK HTTP status: %d", resp.StatusCode)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't read the body: %w", err)
+	}
+
+	var rssfeed RSSFeed
+	if err := xml.Unmarshal(body, &rssfeed); err != nil {
+		return nil, fmt.Errorf("Couldn't unmarshall the body: %w", err)
+	}
+
+	rssfeed.Channel.Title = html.UnescapeString(rssfeed.Channel.Title)
+	rssfeed.Channel.Description = html.UnescapeString(rssfeed.Channel.Description)
+
+	for i := range rssfeed.Channel.Item {
+		rssfeed.Channel.Item[i].Title = html.UnescapeString(rssfeed.Channel.Item[i].Title)
+		rssfeed.Channel.Item[i].Description = html.UnescapeString(rssfeed.Channel.Item[i].Description)
+	}
+
+	return &rssfeed, nil
+}
